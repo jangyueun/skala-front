aef9b0a60a7ebe1f995550d159a17a8c1ae31776
jangyueun <dbdms7773@naver.com>
2026-07-18 19:15:35 +0900

    modify design

--- script/realtimeInfo.js
+++ script/realtimeInfo.js
@@ -21,7 +21,7 @@ citySelect.addEventListener('change', async function(event) {
 
     if (weatherInfo) {
         weatherBox.innerHTML = `
-            <div style="background-color: #e8f8f5; border-left: 5px solid #16a085; padding: 15px; margin-top: 10px;">
+            <div style="background-color: #FFF9FA; border: 1px solid #FFE4E1; padding: 15px; margin-top: 10px; border-radius: 12px;">
                 <h4>모듈형 날씨 피드: ${cityName}</h4>
                 <p>🌡️ 기온: ${weatherInfo.temp}°C</p>
                 <p>💧 습도: ${weatherInfo.humidity}%</p>
